5e48a02ed390cfe2e14944cfcdeda1b8316bca2f
Suganth V <suganth.v2024cse@sece.ac.in>
2026-06-03 18:04:20 +0530

    Project added

--- main.py
+++ main.py
@@ -2,19 +2,28 @@ from diffusers import StableDiffusionPipeline
 import torch
 import matplotlib.pyplot as plt
 
-pipe  = StableDiffusionPipeline.from_pretrained(
+pipe = StableDiffusionPipeline.from_pretrained(
     "runwayml/stable-diffusion-v1-5",
-    torch_dtype=torch.float16
-).to("cuda")
+    torch_dtype=torch.float16,
+    use_safetensors=True
+)
+
+pipe.enable_attention_slicing()
+pipe.enable_vae_slicing()
+
+pipe = pipe.to("cuda")
 
 prompt = input("Enter your prompt: ")
 
-with torch.autocast("cuda"):
-  image=pipe(prompt).images[0]
+image = pipe(
+    prompt,
+    num_inference_steps=20,
+    height=512,
+    width=512
+).images[0]
 
 plt.imshow(image)
 plt.axis("off")
-plt.title("Generate by Stable Diffuser")
 plt.show()
 
 image.save("result/generated_image.png")
